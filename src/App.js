c12cad04aa68a41fbaa3ea8aa0de43af21f4e3b9
Eli Mullan <EliMullan21@gmail.com>
2022-04-05 21:40:44 +0100

    finished project

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div>      
         <Navigation />
-        <Route  path='/:type/:id'>
-          <PetDetailsPage />
+        <Switch>
+        <Route path='/pet-details-not-found'>
+            <PetDetailsNotFound  />
         </Route>
-        <Route path='/:type?'> 
-            <HomePage />
+        <Route  path ='/search'>
+          <SearchPage  />
         </Route>
+          <Route  path='/:type/:id'>
+            <PetDetailsPage />
+          </Route>
+          <Route path='/:type?'> 
+              <HomePage />
+          </Route>
+         
+        </Switch>
       </div>
     </Router>
   );
